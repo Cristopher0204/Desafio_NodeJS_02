25fb80bc58d5165fb775b91c050d47be238a88f8
cristopher0204 <christophersilva954240@gmail.com>
2022-03-01 13:03:02 -0300

    foi dado uma variavel const em cada uma das pessoas foi separado nome,sobrenome,idade,peso,altura e boolean que é verdadeiro
    depos foi dado um console.log em cada um deles separando o que tinha que ser feito.

--- src/index.ts
+++ src/index.ts
@@ -39,48 +39,46 @@ app.listen(PORT, () => {
 
 	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
 	// R01, R02, R03, R04, R05
-	
+});
 	console.log("desafio 02 - Starts...");
 	
-	const nome1 =('cristopher silva');
-	const idade1 = 21;
-	const peso1 = 71;
-	const altura1 = 1.70;
-	const ehdev1 = true;
-	
-	const nome2 = ('leandro silva');
-	const idade2 = 23;
-	const peso2 = 69;
-	const altura2 = 1.65;
-	const ehdev2 = true;
-	
-	const nome3 = ('yago silva');
-	const idade3 = 25;
-	const peso3 = 75;
-	const altura3 = 1.78;
-	const ehdev3 = true;
-	
-	const nome4 = ('vitor silva');
-	const idade4 = 30;
-	const peso4 = 80;
-	const altura4 = 1.80;
-	const ehdev4 = true;
+	 const nome1= ('cristopher'),
+	  sobrenome1= ('silva'),
+	 idade1= 21,
+	 peso1= 71,
+	 altura1= 1.74,
+	 ehdev1= true;
 
-console.log(nome1,idade1,peso1,altura1,ehdev1);
-console.log(nome2,idade2,peso2,altura2,ehdev2);
-console.log(nome3,idade3,peso3,altura3,ehdev3);
-console.log(nome4,idade4,peso4,altura4,ehdev4);
+	 const nome2= ('leandro'),
+	 sobrenome2= ('silva'),
+	 idade2= 23,
+	 peso2= 70,
+	 altura2= 1.70,
+	 ehdev2= true;
 
+	 const nome3= ('yago'),
+	 sobrenome3= ('silva'),
+	 idade3= 25,
+	 peso3= 75,
+	 altura3= 1.78,
+	 ehdev3= true;
+		
+	const nome4= ('vitor'),
+	 sobrenome4= ('silva'),
+	 idade4= 30,
+	 peso4= 80,
+	 altura4= 1.75,
+     ehdev4= true;
 
-let R01idade1 = (21 + 23 + 25 + 30);
-let R02nome = ('cristopher silva, leandro silva, yago silva,vitor silva');
-let R03imc1= 71 / 1.70 ** 2;
-let R03imc2= 69 / 1.65 ** 2;
-let R03imc3= 75 / 1.78 ** 2;
-let R03imc4= 80 / 1.80 ** 2;
-
-console.log(R01idade1);
-console.log(R02nome);
-console.log(R03imc1,R03imc2,R03imc3,R03imc4);
-
-});
+    console.log(nome1,sobrenome1,idade1,peso1,altura1,ehdev1);
+	console.log(nome2,sobrenome2,idade2,peso2,altura2,ehdev2);
+	console.log(nome3,sobrenome3,idade3,peso3,altura3,ehdev3);
+	console.log(nome4,sobrenome4,idade4,peso4,altura4,ehdev4);
+	console.log(idade1+idade2+idade3+idade4);
+	console.log(nome1,nome2,nome3,nome4);
+	console.log(ehdev1,ehdev2,ehdev3,ehdev4);
+	console.log(sobrenome1,sobrenome2,sobrenome3,sobrenome4);
+	console.log(idade1 * altura1 / 2);
+	console.log(idade2 * altura2 / 2);
+	console.log(idade3 * altura3 / 2);
+	console.log(idade4 * altura4 / 2);
