4e4d20ed14073aa0c5cb8d42ebb75d97455cfbc1
cristopher0204 <christophersilva954240@gmail.com>
2022-03-01 19:47:35 -0300

    usei o if para ver se era verdadeiro ou falso depois dei um console. log usei **2 que represente o numero vezes o memso numero .
    para calcular em media o imc de cada um ddepois dando um console .log.

--- src/index.ts
+++ src/index.ts
@@ -41,12 +41,16 @@ app.listen(PORT, () => {
 	// R01, R02, R03, R04, R05
 });
 	console.log("desafio 02 - Starts...");
-	
+
 	 const nome1:string= ('cristopher silva');
 	 const idade1:number= 21;
 	 const peso1:number= 71;
 	 const altura1:number= 1.74;
 	 const ehdev1:boolean= true;
+    
+	 if (ehdev1==true){
+		 console.log(ehdev1)
+	 };
 
 	 const nome2:string= ('leandro silva');
 	 const idade2:number= 23;
@@ -54,30 +58,31 @@ app.listen(PORT, () => {
 	 const altura2:number= 1.70;
 	 const ehdev2:boolean= true;
 
+     if (ehdev2==true){
+      console.log(ehdev2)
+     };
+
 	 const nome3:string= ('yago silva');
 	 const idade3:number= 25;
 	 const peso3:number= 75;
 	 const altura3:number= 1.78;
 	 const ehdev3:boolean= true;
+	 if (ehdev3==true){
+		console.log(ehdev3)
+		};
 		
 	 const nome4:string= ('vitor silva');
 	 const idade4:number= 30;
 	 const peso4:number= 80;
 	 const altura4:number= 1.75;
      const ehdev4:boolean= true;
-
-    console.log(nome1,idade1,peso1,altura1,ehdev1);
-	console.log(nome2,idade2,peso2,altura2,ehdev2);
-	console.log(nome3,idade3,peso3,altura3,ehdev3);
-	console.log(nome4,idade4,peso4,altura4,ehdev4);
-	console.log(idade1+idade2+idade3+idade4);
-	console.log(nome1,nome2,nome3,nome4);
-	
-	console.log(idade1 * altura1 / 2);
-	console.log(idade2 * altura2 / 2);
-	console.log(idade3 * altura3 / 2);
-	console.log(idade4 * altura4 / 2);
-	console.log(ehdev1 === ehdev1);
-    console.log(ehdev2 === ehdev2);
-	console.log(ehdev3 === ehdev3);
-	console.log(ehdev4 === ehdev4);
+	 if (ehdev4==true){
+		console.log(ehdev4)
+		};
+	 
+     console.log(idade1+idade2+idade3+idade4);
+     console.log(nome1,nome2,nome3,nome4);
+     console.log(nome1,peso1 / (altura1**2));
+     console.log(nome2,peso2 / (altura2**2));
+     console.log(nome3,peso3 / (altura3**2));
+     console.log(nome4,peso4 / (altura4**2));
